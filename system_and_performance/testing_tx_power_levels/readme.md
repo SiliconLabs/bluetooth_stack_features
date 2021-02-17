ebc1c8cc013d1ac73d9b8a8246755620e18662d1
FSOFT-HOPNV1 <76993461+FSOFT-HOPNV1@users.noreply.github.com>
2021-02-17 15:15:37 +0700

    new code example: Testing Tx Power Levels

---
+++ system_and_performance/testing_tx_power_levels/readme.md
@@ -0,0 +1,61 @@
+# Testing TX Power Levels #
+ 
+## Description ##
+ 
+The command **sl_bt_system_set_tx_power** can be used to set the global minimum and maximum radiated TX power levels for Bluetooth, but the value which is actually set by the Bluetooth stack may not be the same as the input to the command. The actual set value can be read from the command response.
+
+This example iterates through a power level range between a minimum and a maximum value given to **sl_bt_system_set_tx_power** command and prints out both the input parameter and the response.
+
+> **NOTE:** TX power can be set with 0.1 dBm granularity. For example, the value of 50 equals +5 dBm. For more information, see the description of the [sl_bt_system_set_tx_power](https://docs.silabs.com/bluetooth/3.1/group-sl-bt-system#ga19566861d594376a52e2e1e7481bef68) API command.
+
+This code example has related User's Guides, which may be worth reading before. Find them here:
+
+[Bluetooth TX Power Settings](https://docs.silabs.com/bluetooth/latest/general/system-and-performance/bluetooth-tx-power-settings)
+
+[Current consumption variation with TX Power](https://docs.silabs.com/bluetooth/latest/general/system-and-performance/current-consumption-variation-with-tx-power)
+
+[TX Power Limitations for Regulatory Compliance (ETSI, FCC)](https://docs.silabs.com/bluetooth/latest/general/system-and-performance/tx-power-limitations-for-regulatory-compliance-etsi-fcc)
+
+## Gecko SDK version ##
+ 
+GSDK v3.1.1
+ 
+## Hardware Required ##
+ 
+- A WSTK board.
+
+- A Bluetooth capable radio board, e.g: BRD4161A.
+ 
+## Setup ##
+
+To be able to test this example do as follows:
+
+1. Create a **Bluetooth - SoC Empty** project.
+
+2. Copy the attached app.c file into your project (overwriting existing app.c).
+
+3. Install the software components to use the **VCOM** port (UART) for logging:
+
+- Open the .slcp file in the project.
+
+- Select the SOFTWARE COMPONENTS tab.
+
+- Install **IO Stream: USART** component with the default instance name: **vcom**.
+
+![vcom_component](images/vcom_component.png)
+
+- Find the **Board Control** component  and enable *Virtual COM UART* under its configuration.
+
+- Install the **Log** component (found under Bluetooth > Utility group).
+
+4. Build and flash the project to your device.
+
+5. Do not forget to flash a bootloader to your board, if you have not done so already.
+ 
+## How It Works ##
+ 
+1. On your PC launch the Console that is integrated on Simplicity Studio or can use a third-party terminal tool like TeraTerm to receive the data from the virtual COM port.
+
+2. Reset your device and observe the log messages. As shown, the TX power values of EFR32xG chip set are not continuous, the lower the value, the coarser the adjustment.
+
+![](images/log.png)
---
+++ system_and_performance/testing_tx_power_levels/src/app.c
@@ -0,0 +1,134 @@
+/***************************************************************************//**
+ * @file
+ * @brief Core application logic.
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2020 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of Silicon Labs Master Software License
+ * Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement. This
+ * software is distributed to you in Source Code format and is governed by the
+ * sections of the MSLA applicable to Source Code.
+ *
+ ******************************************************************************/
+#include "em_common.h"
+#include "sl_app_assert.h"
+#include "sl_bluetooth.h"
+#include "gatt_db.h"
+#include "app.h"
+#include "sl_app_log.h"
+
+#define MIN_TEST_NUMBER         (-300) // This is the minimum tx_power to start the sweep.
+#define MAX_TEST_NUMBER         (80)   // This is the maximum tx_power to end the sweep.
+
+static int16_t tx_set;
+static int16_t tx_to_set = MIN_TEST_NUMBER;
+
+/**************************************************************************//**
+ * Application Init.
+ *****************************************************************************/
+SL_WEAK void app_init(void)
+{
+  /////////////////////////////////////////////////////////////////////////////
+  // Put your additional application init code here!                         //
+  // This is called once during start-up.                                    //
+  /////////////////////////////////////////////////////////////////////////////
+}
+
+/**************************************************************************//**
+ * Application Process Action.
+ *****************************************************************************/
+SL_WEAK void app_process_action(void)
+{
+  /////////////////////////////////////////////////////////////////////////////
+  // Put your additional application code here!                              //
+  // This is called infinitely.                                              //
+  // Do not call blocking functions from here!                               //
+  /////////////////////////////////////////////////////////////////////////////
+}
+
+/**************************************************************************//**
+ * Bluetooth stack event handler.
+ * This overrides the dummy weak implementation.
+ *
+ * @param[in] evt Event coming from the Bluetooth stack.
+ *****************************************************************************/
+void sl_bt_on_event(sl_bt_msg_t *evt)
+{
+  sl_status_t sc;
+  bd_addr address;
+  uint8_t address_type;
+
+  switch (SL_BT_MSG_ID(evt->header)) {
+    // -------------------------------
+    // This event indicates the device has started and the radio is ready.
+    // Do not call any stack command before receiving this boot event!
+    case sl_bt_evt_system_boot_id:
+      /* Print stack version. */
+      sl_app_log("Bluetooth stack booted: v%d.%d.%d-b%d\n",
+                 evt->data.evt_system_boot.major,
+                 evt->data.evt_system_boot.minor,
+                 evt->data.evt_system_boot.patch,
+                 evt->data.evt_system_boot.build);
+
+      // Extract unique ID from BT Address.
+      sc = sl_bt_system_get_identity_address(&address, &address_type);
+      sl_app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to get Bluetooth address\n",
+                    (int)sc);
+
+      /* Print local Bluetooth address. */
+      sl_app_log("Bluetooth %s address: %02X:%02X:%02X:%02X:%02X:%02X\n",
+                 address_type ? "static random" : "public device",
+                 address.addr[5],
+                 address.addr[4],
+                 address.addr[3],
+                 address.addr[2],
+                 address.addr[1],
+                 address.addr[0]);
+
+      sl_app_log("------------------------Start-------------------------\r\n");
+
+      /* Set a timer to call sl_bt_system_set_tx_power every 10 ms
+       * with incremental input and print out result. */
+      sc = sl_bt_system_set_soft_timer(328, 1, 0);
+      sl_app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to start a software timer\r\n",
+                    (int)sc);
+      break;
+
+    case sl_bt_evt_system_soft_timer_id:
+
+      if (evt->data.evt_system_soft_timer.handle == 1) {
+
+        /* Set maximum tx power and read what was actually set */
+        sc = sl_bt_system_set_tx_power(MIN_TEST_NUMBER, tx_to_set, NULL, &tx_set);
+        sl_app_assert(sc == SL_STATUS_OK,
+                      "[E: 0x%04x] Failed to set tx power\r\n",
+                      (int)sc);
+        /* Print out input value and response from the command.
+         * The print formatting can be changed as desired
+         * (e.g. so that a log can be imported as csv into excel).
+         */
+        sl_app_log("set_tx_power(%03d) returns %03d\r\n", tx_to_set, tx_set);
+
+        if (tx_to_set++ == MAX_TEST_NUMBER) {
+          /* If max value has been reached print "End" and stop timer */
+          sl_app_log("-----------------------End-------------------------\r\n");
+          sc = sl_bt_system_set_soft_timer(0, 1, 0);
+          sl_app_assert(sc == SL_STATUS_OK,
+                        "[E: 0x%04x] Failed to stop a software timer\r\n",
+                        (int)sc);
+        }
+      }
+      break;
+
+    // -------------------------------
+    // Default event handler.
+    default:
+      break;
+  }
+}

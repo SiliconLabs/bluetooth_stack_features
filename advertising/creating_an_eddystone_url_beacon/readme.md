b0234de9febf04e57ae2cdddbe7f4b9730904ecb
FSOFT-HOPNV1 <76993461+FSOFT-HOPNV1@users.noreply.github.com>
2021-03-18 23:19:21 +0700

    Code example added: Creating an Eddystone URL beacon

---
+++ advertising/creating_an_eddystone_url_beacon/readme.md
@@ -0,0 +1,104 @@
+# Creating an Eddystone-URL Beacon #
+ 
+## Description ##
+ 
+This example demonstrates how to create an Eddystone beacon with Silicon Labs' Bluetooth SDK.
+
+### Eddystone Beacons ###
+
+The [Eddystone format](https://developers.google.com/beacons/eddystone) defines four different packet types. This example shows how to implement the Eddystone-URL packet. The Eddystone-URL packet advertises a URL in a compressed format. For more information on Eddystone beacons, see [Google's Eddystone repository](https://github.com/google/eddystone/tree/master/eddystone-url).
+
+### URL Beacon Implementation ###
+
+The first step in creating the beacon is to define custom user data to be advertised:
+
+``` C
+static const uint8_t eddystone_data[] = {
+  0x03,          // Length of service list
+  0x03,          // Service list
+  0xAA, 0xFE,    // Eddystone ID
+  0x10,          // Length of service data
+  0x16,          // Service data
+  0xAA,  0xFE,   // Eddystone ID
+  0x10,          // Frame type Eddystone-URL
+  0x00,          // Tx power
+  0x00,          // 0x00=http://www., 0x01=https://www.
+  's','i','l','a','b','s','.','c','o','m'
+};
+```
+
+Call the API to set this data to be advertised.
+
+``` C
+sl_bt_advertiser_set_data(advertising_set_handle, 0, sizeof(eddystone_data), eddystone_data);
+```
+
+The first two parameters are the advertising set handle and a value indicating that data is to be used in advertising packets.
+
+Then, set the transmit power. The Eddystone-URL packet format includes the transmit power in the 10th byte. This data can be used by receivers of the beacon to determine distance so it is important that this data and the actual transmit power match.
+
+``` C
+sl_bt_system_set_tx_power(0, 0, &ret_min_power, &ret_max_power); // Set global minimum and maximum TX power to 0 dBm
+```
+
+The following call sets the minimum and maximum advertising interval to 100 ms ( 625 us * 160). All three advertising channels are used by default (37, 38, 39).
+
+``` C
+sl_bt_advertiser_set_timing(advertising_set_handle, 160, 160, 0, 0);
+```
+
+The device is now ready to start advertising the Eddystone-URL beacon data. This is accomplished by making the following call:
+
+``` C
+sl_bt_advertiser_start(advertising_set_handle, advertiser_user_data, advertiser_non_connectable);
+```
+This tells the stack to use the custom user data and to make the beacon non-connectable.
+ 
+## Gecko SDK version ##
+ 
+GSDK v3.1.1
+ 
+## Hardware Required ##
+ 
+- A WSTK board.
+- A Bluetooth capable radio board, e.g: BRD4161A.
+ 
+## Setup
+ 
+To be able to test this example do as follows:
+
+1. Create a **Bluetooth - SoC Empty** project.
+
+2. Copy the attached app.c file into your project (overwriting existing app.c).
+
+3. Install the software components to use the **VCOM** port (UART) for logging:
+
+- Open the .slcp file in the project.
+
+- Select the SOFTWARE COMPONENTS tab.
+
+- Install **IO Stream: USART** component with the default instance name: **vcom**.
+
+![vcom_component](images/vcom_component.png)
+
+- Find the **Board Control** component  and enable *Virtual COM UART* under its configuration.
+
+- Install the **Log** component (found under Bluetooth > Utility group).
+
+4. Build and flash the project to your device.
+
+5. Do not forget to flash a bootloader to your board, if you have not done so already.
+ 
+## How It Works ##
+ 
+Follow the below steps to test the example:
+
+1. Open the EFR Connect app on your smartphone.
+
+2. Find your device in the Bluetooth Browser, advertising as Empty Example, and observe the advertising data.
+
+![](images/efr_connect.PNG)
+
+You can launch the Console that is integrated on Simplicity Studio or can use a third-party terminal tool like TeraTerm to receive the logs from the virtual COM port.
+
+![](images/console.png)
---
+++ advertising/creating_an_eddystone_url_beacon/src/app.c
@@ -0,0 +1,176 @@
+/***************************************************************************//**
+ * @file
+ * @brief Core application logic.
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2020 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of Silicon Labs Master Software License
+ * Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement. This
+ * software is distributed to you in Source Code format and is governed by the
+ * sections of the MSLA applicable to Source Code.
+ *
+ ******************************************************************************/
+#include "em_common.h"
+#include "sl_app_assert.h"
+#include "sl_bluetooth.h"
+#include "gatt_db.h"
+#include "app.h"
+#include "sl_app_log.h"
+
+// The advertising set handle allocated from Bluetooth stack.
+static uint8_t advertising_set_handle = 0xff;
+
+static const uint8_t eddystone_data[] = {
+  0x03,          // Length of service list
+  0x03,          // Service list
+  0xAA, 0xFE,    // Eddystone ID
+  0x10,          // Length of service data
+  0x16,          // Service data
+  0xAA,  0xFE,   // Eddystone ID
+  0x10,          // Frame type Eddystone-URL
+  0x00,          // Tx power
+  0x01,          // 0x00=http://www., 0x01=https://www.
+  's','i','l','a','b','s','.','c','o','m'
+};
+
+/**************************************************************************//**
+ * Application Init.
+ *****************************************************************************/
+SL_WEAK void app_init(void)
+{
+  /////////////////////////////////////////////////////////////////////////////
+  // Put your additional application init code here!                         //
+  // This is called once during start-up.                                    //
+  /////////////////////////////////////////////////////////////////////////////
+}
+
+/**************************************************************************//**
+ * Application Process Action.
+ *****************************************************************************/
+SL_WEAK void app_process_action(void)
+{
+  /////////////////////////////////////////////////////////////////////////////
+  // Put your additional application code here!                              //
+  // This is called infinitely.                                              //
+  // Do not call blocking functions from here!                               //
+  /////////////////////////////////////////////////////////////////////////////
+}
+
+/**************************************************************************//**
+ * Bluetooth stack event handler.
+ * This overrides the dummy weak implementation.
+ *
+ * @param[in] evt Event coming from the Bluetooth stack.
+ *****************************************************************************/
+void sl_bt_on_event(sl_bt_msg_t *evt)
+{
+  sl_status_t sc;
+  bd_addr address;
+  uint8_t address_type;
+  uint8_t system_id[8];
+  int16_t ret_min_power, ret_max_power;
+
+  switch (SL_BT_MSG_ID(evt->header)) {
+    // -------------------------------
+    // This event indicates the device has started and the radio is ready.
+    // Do not call any stack command before receiving this boot event!
+    case sl_bt_evt_system_boot_id:
+      /* Print stack version */
+      sl_app_log("Bluetooth stack booted: v%d.%d.%d-b%d\n",
+                 evt->data.evt_system_boot.major,
+                 evt->data.evt_system_boot.minor,
+                 evt->data.evt_system_boot.patch,
+                 evt->data.evt_system_boot.build);
+      // Extract unique ID from BT Address.
+      sc = sl_bt_system_get_identity_address(&address, &address_type);
+      sl_app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to get Bluetooth address\n",
+                    (int)sc);
+
+      /* Print Bluetooth address */
+      sl_app_log("Bluetooth %s address: %02X:%02X:%02X:%02X:%02X:%02X\n",
+                       address_type ? "static random" : "public device",
+                       address.addr[5],
+                       address.addr[4],
+                       address.addr[3],
+                       address.addr[2],
+                       address.addr[1],
+                       address.addr[0]);
+
+      // Pad and reverse unique ID to get System ID.
+      system_id[0] = address.addr[5];
+      system_id[1] = address.addr[4];
+      system_id[2] = address.addr[3];
+      system_id[3] = 0xFF;
+      system_id[4] = 0xFE;
+      system_id[5] = address.addr[2];
+      system_id[6] = address.addr[1];
+      system_id[7] = address.addr[0];
+
+      sc = sl_bt_gatt_server_write_attribute_value(gattdb_system_id,
+                                                   0,
+                                                   sizeof(system_id),
+                                                   system_id);
+      sl_app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to write attribute\n",
+                    (int)sc);
+
+      // Create an advertising set.
+      sc = sl_bt_advertiser_create_set(&advertising_set_handle);
+      sl_app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to create advertising set\n",
+                    (int)sc);
+
+      // Set 0 dBm Transmit Power.
+      sc = sl_bt_system_set_tx_power(0, 0, &ret_min_power, &ret_max_power);
+      sl_app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to set max TX power for Bluetooth\n",
+                    (int)sc);
+      (void)ret_min_power;
+      (void)ret_max_power;
+
+      // Set custom advertising data.
+      sc = sl_bt_advertiser_set_data(advertising_set_handle,
+                                     0,
+                                     sizeof(eddystone_data),
+                                     eddystone_data);
+      sl_app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to set advertiser data\n",
+                    (int)sc);
+
+      // Set advertising parameters. 100ms advertisement interval.
+      sc = sl_bt_advertiser_set_timing(
+        advertising_set_handle,
+        160, // min. adv. interval (milliseconds * 1.6)
+        160, // max. adv. interval (milliseconds * 1.6)
+        0,   // adv. duration
+        0);  // max. num. adv. events
+      sl_app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to set advertising timing\n",
+                    (int)sc);
+
+      // Start advertising in user mode and disable connections.
+      sc = sl_bt_advertiser_start(
+        advertising_set_handle,
+        advertiser_user_data,
+        advertiser_non_connectable);
+      sl_app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to start advertising\n",
+                    (int)sc);
+      sl_app_log("boot event - starting advertising\r\n");
+      break;
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Add additional event handlers here as your application requires!      //
+    ///////////////////////////////////////////////////////////////////////////
+
+    // -------------------------------
+    // Default event handler.
+    default:
+      break;
+  }
+}

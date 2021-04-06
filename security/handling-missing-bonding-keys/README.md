3cc649243515c046a15396e966cba524d50500d3
FSOFT-VINHPT9 <79197973+FSOFT-VINHPT9@users.noreply.github.com>
2021-04-07 02:37:32 +0700

    code example added: Handling Missing Bonding Keys

---
+++ security/handling-missing-bonding-keys/README.md
@@ -0,0 +1,182 @@
+# Handling Missing Bonding Keys #
+
+## Description ##
+
+This example demonstrates two scenarios that can occur after a bond has been established between a smartphone and [a Bluetooth-Capable EFR32](https://www.silabs.com/products/development-tools/wireless/wireless-radio-boards#bluetooth).
+
+**Case 1:** Bonding keys are deleted from the smartphone.
+* Potential causes include manually deleting the keys, software/factory reset, app malfunction.
+
+**Case 2:** Bonding keys are deleted from the EFR32.
+* Potential causes include a full chip erase, running out of space for storing bonding keys (in which case the older keys can be deleted)
+
+As an introduction to Bluetooth Security features, read [Using Bluetooth security features in Silicon Labs Bluetooth SDK](https://docs.silabs.com/bluetooth/2.13/general/security/using-bluetooth-security-features-in-silicon-labs-bluetooth-sdk).
+
+The following are the most relevant Bluetooth API commands used in the sample code:
+
+[sl_bt_sm_set_passkey(-1);](https://docs.silabs.com/bluetooth/3.1/group-sl-bt-sm#ga3ea16a5b602a40ba4d98030e823067e3)
+
+* *Used to generate a random passkey required to support Level 4 security.*
+
+[sl_bt_sm_configure(0x0B,sm_io_capability_displayonly);](https://docs.silabs.com/bluetooth/3.1/group-sl-bt-sm#ga2668706f7ffb70681ed974f1e0b46c86)
+
+* *0x0B: Enables everything except the “secure connections only flag” bit.*
+* *displayonly: Used to represent the EFR as a display only device.*
+
+[sl_bt_sm_set_bondable_mode(1);](https://docs.silabs.com/bluetooth/3.1/group-sl-bt-sm#ga5b300b3007067d4671ecf598752d2f60)
+
+* *Set the device in bondable mode.*
+
+[sl_bt_sm_increase_security(connectionhandle);](https://docs.silabs.com/bluetooth/3.1/group-sl-bt-sm#gadde7d3d515ab2831e5cd40898530f874)
+
+* Used to increase security level after connection is established.
+
+More details of all these commands can be found in [Bluetooth API Reference](https://docs.silabs.com/bluetooth/3.1/group-sl-bt-sm) guide.
+
+**Disclaimer:** This document and the sample application attached with it is to be used for information, reference about the topic and testing. It is not in any way production ready code. The use of the code will be at your own risk.
+
+
+## Gecko SDK version ##
+
+GSDK v3.1.1
+
+## Hardware Required ##
+
+- One WSTK board
+- One Bluetooth capable radio board, e.g: BRD4162A
+
+## Setting up ##
+
+Central: A smartphone with the EFR Connect app
+
+1. An Android smartphone (A Sony Xperia Z1 running Android 5.1.1 OS, **doesn’t support LE secure connections**)
+
+2. Another Android smartphone (A Samsung Galaxy S6 Edge running Android 7.0 OS, **supports LE secure connections**)
+
+3. An Apple smartphone (iPhone 6, running iOS 11.2.6 **supports LE secure connections**)
+
+Peripheral: An EFR32 Radio board (a BGM module in this case, **it supports LE secure connections**) (Programmed with Bluetooth SDK 3.1.1)
+
+1. Create a **Bluetooth - SoC Empty** project in Simplicity Studio.
+
+2. The function code is in the *app.c* file. Copy and replace this file to the project folder.
+
+3. Install the software components to use the **VCOM** port (UART) for logging:
+   - Open the .slcp file in the project.
+
+   - Select the SOFTWARE COMPONENTS tab.
+
+   - Install **IO Stream: USART** component with the default instance name: **vcom**  
+    ![install usart](images/install_usart.png)
+
+   - Find the **Board Control** component and click to the **Configure** button like below
+    ![board control configure](images/board_control_configure.png)  
+    Then enable *Virtual COM UART* under its configuration
+    ![enable vcom](images/enable_vir_com.png)
+
+   - Install the **Log** component (found under Bluetooth > Utility group)
+   ![log configure](images/log.png)
+
+   - Create the first button by installing the **Simple Button** component with the default instance name: **btn0**
+   ![button0](images/btn0.png)  
+   Create the second button by click the **Add New Instances** button with the default instance name: **btn1**
+   ![button1](images/btn1.png)
+
+4. Import the GATT configuration:        
+    - Open the **Bluetooth GATT Configurator** under the **CONFIGURATION TOOLS** tab.
+    - Find the Import button and import the attached **gatt_configuration.btconf** file.
+    ![btc configure](images/btconf.png)
+    - Save the GATT configuration (Ctrl+S).
+
+5. Compile and flash. The output will be shown on the Serial Port.
+
+
+
+## Usage ##
+
+### Android ##
+
+According to our observation, Android OS treats pairing and connecting as two separate events. Pairing is optional if your GATT database does not contain any characteristic that requires encryption or bonding. You can connect, not pair and still be able to read and write other characteristics. A connection made between the EFR and Smartphone is security level 1. When paired, security  level increases to 3 or 4 depending on Legacy or Secure connections respectively.
+
+<img src="images/figure_1.png" width="378" height="672">
+
+*Figure 1: The Bluetooth Browser Showing the Empty Example Device*
+
+
+<img src="images/figure_2.png" width="378" height="672">
+
+*Figure 2: Connected but not Bonded to the EFR*
+
+
+
+<img src="images/figure_3.png" width="378" height="672">
+
+*Figure 3: Optional Pairing/Bonding Request Received*
+
+
+
+### Apple ###
+
+iOS actively asks for a pairing request if your GATT database contains any characteristic that require encryption or bonding because it reads all the characteristics as soon as you connect a device. As a result, you receive an active pairing request while setting up a connection. The request is initiated with security level 1 but is increased up to level 4 once a bond is created successfully.
+
+<img src="images/figure_4.png" width="236" height="467">
+
+*Figure 4: Bluetooth Browser in iOS*
+
+
+
+<img src="images/figure_5.png" width="236" height="467">
+
+*Figure 5: Active Pairing/Bonding Request Received while Connecting*
+
+
+
+### Observations ###
+
+These observations are for the flags set to values as mentioned above in the [Description](#description). These will vary based on the values and device capabilities as mentioned in [Using Bluetooth Security Features in Silicon Labs Bluetooth SDK](https://docs.silabs.com/bluetooth/2.13/general/security/using-bluetooth-security-features-in-silicon-labs-bluetooth-sdk).
+
+**Scenario 1: Bonding keys are lost on the smartphone.**
+
+iPhone: A new pairing request is initiated with a request to enter the passkey. Upon successful pairing, security Level_4 is achieved. The EFR overwrites the existing bonding keys.
+
+
+
+Android (both test devices): Android phones receive two new pairing requests. The first one with a yes(pair)/no option to pair. The second one asks for a passkey entry. Upon successful pairing security Level_3 is achieved for phones which don’t support secure connections. Security Level_4 is achieved for phones which support secure pairing.
+
+
+
+**Scenario 2: Bonding keys are lost on the EFR.**
+
+1. Android EFR Connect App connecting to EFR
+
+   a. On LE secure supported phones: Pairing fails for a reason
+
+   ​	**0x1006**: pin_or_key_missing; Pairing failed because of missing PIN, or authentication failed because of missing key. The connection remains open with security Level_1. This error can be caught, and an increase security function (sm_increase_security; refer section ) can be triggered. As a result, a passkey entry request is received on the phone. Upon successful pairing, security Level_4 is achieved.
+
+   b. On phones which don’t support LE secure: Again, pairing fails for a reason
+
+   ​	**0x1006**: pin_or_key_missing; Pairing failed because of missing PIN, or authentication failed because of a missing key. The connection is terminated by the phone upon detecting a failed attempt and the bonding keys are deleted on the phone. An error code **0x1013:** remote user terminated is received. A repeated pairing attempt sets up new bonding keys on both sides as if they are connecting for the first time.
+
+​    		* This behavior can change based on your phone/Android version and hardware configurations of your phone.
+
+
+2. iOS App connecting to EFR
+
+   Pairing fails with an error code **0x1006** (Pin or key missing). The connection remains open with security Level_1. This error can be caught in the *sl_bt_evt_sm_bonding_failed_id*, and an increase security function can be triggered. This command fails because of a timeout with error code **0x0007**after about 15-30 seconds. This error is caught and an increase security function *sl_bt_sm_increase_security(connectionhandle)* triggers a new passkey request. Upon successful pairing, security Level_4 is achieved.
+
+
+### Testing the Example ###
+
+The sample application attached with this document demonstrates these mechanisms for handling missing bonding keys on both sides. Create the project following the instructions in the attached readme file and flash it to a WSTK. This example prints to the WSTK VCOM so connect a terminal to the WSTK's COM Port on your PC to see the messages.
+
+1. Connect to the EFR32 named "Empty Example" from the EFR Connect App Bluetooth Browser. Enter the passkey displayed on the console when asked.
+
+2. Disconnect. Delete bonding data from your phone settings> Bluetooth. Reconnect from the app. Note the observations on the serial monitor.
+
+3. Press reset on EFR32. **The sample application attached with this KBA is designed to delete the bonding keys when you reset**. **IT IS NOT RECOMMENDED IN REAL APPLICATIONS**. Follow step 1.
+
+4. Disconnect, press reset on EFR32, and reconnect (do not delete bonding data on the phone this time). Note the observations on the serial monitor.
+
+
+
+Contribution credit: The example code is based on the work of [Théo Meyer](https://www.silabs.com/community/profile.html/home/users/u/u-sOFE4O7OUAo_Lq7g7-/profile) from [www.strataggem.com](http://www.strataggem.com/)
---
+++ security/handling-missing-bonding-keys/src/app.c
@@ -0,0 +1,413 @@
+/***************************************************************************/ /**
+ * @file
+ * @brief Core application logic.
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2020 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of Silicon Labs Master Software License
+ * Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement. This
+ * software is distributed to you in Source Code format and is governed by the
+ * sections of the MSLA applicable to Source Code.
+ *
+ ******************************************************************************/
+#include "em_common.h"
+#include "sl_app_assert.h"
+#include "sl_bluetooth.h"
+#include "gatt_db.h"
+#include "app.h"
+#include "sl_simple_button_instances.h"
+#include "sl_app_log.h"
+
+// The advertising set handle allocated from Bluetooth stack.
+static uint8_t advertising_set_handle = 0xff;
+
+uint8_t bleBondingHandle = 0xFF;
+
+#define BSP_LED_OFF (1)
+#define BSP_LED_ON  (0)
+
+uint8_t connectionToIncreasSec = 0xFF;
+
+enum SOFT_TIMER_HANDLES
+{
+  INCREASE_SEC_TMR_HANDLE = 1,
+};
+/**************************************************************************/ /**
+ * Application Init.
+ *****************************************************************************/
+SL_WEAK void app_init(void)
+{
+  /////////////////////////////////////////////////////////////////////////////
+  // Put your additional application init code here!                         //
+  // This is called once during start-up.                                    //
+  /////////////////////////////////////////////////////////////////////////////
+
+  sl_app_log("BLE SECURITY TEST APPLICATION : Starting application log... \r\n");
+  sl_app_log("App init \r\n");
+}
+
+/**************************************************************************/ /**
+ * Application Process Action.
+ *****************************************************************************/
+SL_WEAK void app_process_action(void)
+{
+  /////////////////////////////////////////////////////////////////////////////
+  // Put your additional application code here!                              //
+  // This is called infinitely.                                              //
+  // Do not call blocking functions from here!                               //
+  /////////////////////////////////////////////////////////////////////////////
+}
+
+/**************************************************************************/ /**
+ * Bluetooth stack event handler.
+ * This overrides the dummy weak implementation.
+ *
+ * @param[in] evt Event coming from the Bluetooth stack.
+ *****************************************************************************/
+void sl_bt_on_event(sl_bt_msg_t *evt)
+{
+  sl_status_t sc;
+  bd_addr address;
+  uint8_t address_type;
+  uint8_t system_id[8];
+  uint8_t i;
+
+  switch (SL_BT_MSG_ID(evt->header))
+  {
+  // -------------------------------
+  // This event indicates the device has started and the radio is ready.
+  // Do not call any stack command before receiving this boot event!
+  case sl_bt_evt_system_boot_id:
+    sl_app_log("stack version: %u.%u.%u\r\r\n", evt->data.evt_system_boot.major, evt->data.evt_system_boot.minor, evt->data.evt_system_boot.patch);
+    // Extract unique ID from BT Address.
+    sc = sl_bt_system_get_identity_address(&address, &address_type);
+    sl_app_assert(sc == SL_STATUS_OK,
+                  "[E: 0x%04x] Failed to get Bluetooth address\n",
+                  (int)sc);
+
+    sl_app_log("local BT device address: ");
+    for (i = 0; i < 5; i++)
+    {
+      sl_app_log("%2.2x:", address.addr[5 - i]);
+    }
+    sl_app_log("%2.2x\r\r\n", address.addr[0]);
+    // Pad and reverse unique ID to get System ID.
+    system_id[0] = address.addr[5];
+    system_id[1] = address.addr[4];
+    system_id[2] = address.addr[3];
+    system_id[3] = 0xFF;
+    system_id[4] = 0xFE;
+    system_id[5] = address.addr[2];
+    system_id[6] = address.addr[1];
+    system_id[7] = address.addr[0];
+
+    sc = sl_bt_gatt_server_write_attribute_value(gattdb_system_id,
+                                                 0,
+                                                 sizeof(system_id),
+                                                 system_id);
+    sl_app_assert(sc == SL_STATUS_OK,
+                  "[E: 0x%04x] Failed to write attribute\r\n",
+                  (int)sc);
+
+    //If you don't want to delete the bonding keys when resetting, keep PB0 and PB1 pressed duing reset.
+    if (sl_button_btn0.get_state(sl_button_btn0.context) && sl_button_btn1.get_state(sl_button_btn1.context))
+    {
+      sl_app_log("Clearing already stored bondings !\r\n");
+      sc = sl_bt_sm_delete_bondings();
+      sl_app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to delete the bonding keys\r\n",
+                    (int)sc);
+    }
+
+    sc = sl_bt_sm_store_bonding_configuration(8, 2);
+    sl_app_assert(sc == SL_STATUS_OK,
+                  "[E: 0x%04x] Failed to set the maximum allowed bonding count and bonding policy\r\n",
+                  (int)sc);
+
+    sc = sl_bt_sm_set_passkey(-1);
+    sl_app_assert(sc == SL_STATUS_OK,
+                  "[E: 0x%04x] Failed to set using random passkeys\r\n",
+                  (int)sc);
+
+    sc = sl_bt_sm_configure(0x0B, sm_io_capability_displayonly);
+    sl_app_assert(sc == SL_STATUS_OK,
+                  "[E: 0x%04x] Failed to configure security requirements and I/O capabilities of the system\r\n",
+                  (int)sc);
+
+    sc = sl_bt_sm_set_bondable_mode(1);
+    sl_app_assert(sc == SL_STATUS_OK,
+                  "[E: 0x%04x] Failed to set the device accept new bondings\r\n",
+                  (int)sc);
+
+    // Create an advertising set.
+    sc = sl_bt_advertiser_create_set(&advertising_set_handle);
+    sl_app_assert(sc == SL_STATUS_OK,
+                  "[E: 0x%04x] Failed to create advertising set\r\n",
+                  (int)sc);
+
+    sc = sl_bt_advertiser_set_timing(
+        advertising_set_handle,
+        1216, // min. adv. interval
+        2056, // max. adv. interval
+        0,    // adv. duration
+        0);   // max. num. adv. events
+    sl_app_assert(sc == SL_STATUS_OK,
+                  "[E: 0x%04x] Failed to set advertising timing\r\n",
+                  (int)sc);
+    // Start general advertising and enable connections.
+    sc = sl_bt_advertiser_start(
+        advertising_set_handle,
+        advertiser_general_discoverable,
+        advertiser_connectable_scannable);
+    sl_app_assert(sc == SL_STATUS_OK,
+                  "[E: 0x%04x] Failed to start advertising\r\n",
+                  (int)sc);
+    break;
+
+  case sl_bt_evt_system_soft_timer_id:
+    switch (evt->data.evt_system_soft_timer.handle)
+    {
+    case INCREASE_SEC_TMR_HANDLE:
+      sl_app_log("Increasing security after delay...\r\n");
+      sc = sl_bt_sm_increase_security(connectionToIncreasSec);
+      sl_app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to enhance the security\r\n",
+                    (int)sc);
+      break;
+    default:
+      break;
+    }
+    break;
+
+  // -------------------------------
+  // This event indicates that a new connection was opened.
+  case sl_bt_evt_connection_opened_id:
+    sl_app_log("Bluetooth Stack Event : CONNECTION OPENED\r\n");
+    sc = sl_bt_advertiser_stop(advertising_set_handle);
+    sl_app_assert(sc == SL_STATUS_OK,
+                  "[E: 0x%04x] Failed to stop the advertising\r\n",
+                  (int)sc);
+
+    // Store the connection ID
+    uint8_t activeConnectionId = 0;
+    activeConnectionId = evt->data.evt_connection_opened.connection;
+    bleBondingHandle = evt->data.evt_connection_opened.bonding;
+
+    if (bleBondingHandle == 0xFF)
+    {
+      sl_app_log("Increasing security\r\n");
+      sc = sl_bt_sm_increase_security(activeConnectionId);
+      sl_app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to enhance the security\r\n",
+                    (int)sc);
+    }
+    else
+    {
+      sl_app_log("Already Bonded (ID: %d)\r\n", bleBondingHandle);
+    }
+    break;
+
+  case sl_bt_evt_connection_parameters_id:
+  {
+    sl_app_log("Bluetooth Stack Event : CONNECTION Parameters ID\r\n");
+    uint8_t connectionhandle = evt->data.evt_connection_parameters.connection;
+    uint16_t txSize = evt->data.evt_connection_parameters.txsize;
+    uint8_t securityLevel = evt->data.evt_connection_parameters.security_mode + 1;
+    uint16_t timeout = evt->data.evt_connection_parameters.timeout;
+    if (securityLevel > 2)
+    {
+      sl_app_log("[OK]      Bluetooth Stack Event : CONNECTION PARAMETERS : MTU = %d, SecLvl : %d, Timeout : %d\r\n", txSize, securityLevel, timeout);
+    }
+    else
+    {
+      sl_app_log("Bluetooth Stack Event : CONNECTION PARAMETERS : MTU = %d, SecLvl : %d, timeout : %d\r\n", txSize, securityLevel, timeout);
+    }
+
+    //If security is less than 2 increase so devices can bond
+    if (securityLevel < 2)
+    {
+      sl_app_log("Bonding Handle is: 0x%04X\r\n", bleBondingHandle);
+      if (bleBondingHandle == 0xFF)
+      {
+        sl_app_log("Increasing security.\r\n");
+        sc = sl_bt_sm_increase_security(connectionhandle);
+        sl_app_assert(sc == SL_STATUS_OK,
+                      "[E: 0x%04x] Failed to enhance the security\r\n",
+                      (int)sc);
+
+        connectionToIncreasSec = connectionhandle;
+      }
+      else
+      {
+        sl_app_log("Increasing security..\r\n");
+        sc = sl_bt_sm_increase_security(connectionhandle);
+        sl_app_assert(sc == SL_STATUS_OK,
+                      "[E: 0x%04x] Failed to enhance the security\r\n",
+                      (int)sc);
+      }
+    }
+    else
+    {
+      sl_app_log("Clear increase sec timer.\r\n");
+      sl_bt_system_set_soft_timer(0, INCREASE_SEC_TMR_HANDLE, 1); //Clear timer
+    }
+    break;
+  }
+
+  // -------------------------------
+  // This event indicates that a connection was closed.
+  case sl_bt_evt_connection_closed_id:
+    bleBondingHandle = 0xFF; //reset bonding handle variable to avoid deleting wrong bonding info
+    sl_app_log("Bluetooth Stack Event : CONNECTION CLOSED (reason: 0x%04X)\r\n", evt->data.evt_connection_closed.reason);
+
+    // Restart advertising after client has disconnected.
+    sc = sl_bt_advertiser_start(
+        advertising_set_handle,
+        advertiser_general_discoverable,
+        advertiser_connectable_scannable);
+    sl_app_assert(sc == SL_STATUS_OK,
+                  "[E: 0x%04x] Failed to start advertising\r\n",
+                  (int)sc);
+    break;
+
+  case sl_bt_evt_sm_passkey_display_id:
+    sl_app_log("Passkey %06ld\r\n", evt->data.evt_sm_passkey_display.passkey);
+    sl_app_log("Bluetooth Stack Event : PASSKEY DISPLAY\r\n");
+    break;
+
+  case sl_bt_evt_sm_confirm_passkey_id:
+    sl_app_log("Bluetooth Stack Event : PASSKEY CONFIRM\r\n");
+    break;
+
+  case sl_bt_evt_sm_passkey_request_id:
+    sl_app_log("Bluetooth Stack Event : PASSKEY REQUEST\r\n");
+    break;
+
+  case sl_bt_evt_sm_confirm_bonding_id:
+    sl_app_log("Bluetooth Stack Event : CONFIRM BONDING\r\n");
+    uint8_t connectionhandle = evt->data.evt_connection_parameters.connection;
+
+    /* This command automatically confirms bonding requests. This is not
+      recommended in real life applications for security purposes.
+      Ideally you want the user to accept the bonding request*/
+    sc = sl_bt_sm_bonding_confirm(connectionhandle, 1);
+    sl_app_assert(sc == SL_STATUS_OK,
+                  "[E: 0x%04x] Failed to confirm bonding requests\r\n",
+                  (int)sc);
+    break;
+
+  case sl_bt_evt_sm_bonded_id:
+    sl_app_log("Bluetooth Stack Event : BONDED\r\n");
+    break;
+
+  case sl_bt_evt_sm_bonding_failed_id:
+  {
+    uint8_t connectionHandle = evt->data.evt_sm_bonding_failed.connection;
+    uint16_t reason = evt->data.evt_sm_bonding_failed.reason;
+    sl_app_log("Bluetooth Stack Event : BONDING FAILED (connection: %d, reason: 0x%04X, bondingHandle: %d)\r\n",
+               connectionHandle, reason, bleBondingHandle);
+    if (reason == SL_STATUS_BT_SMP_PASSKEY_ENTRY_FAILED || reason == SL_STATUS_TIMEOUT)
+    {
+      sl_app_log("Increasing security... because reason is 0x%04x\r\n", reason);
+      sc = sl_bt_sm_increase_security(connectionHandle);
+      sl_app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to enhance the security\r\n",
+                    (int)sc);
+    }
+
+    if (reason == SL_STATUS_BT_SMP_PAIRING_NOT_SUPPORTED || reason == SL_STATUS_BT_CTRL_PIN_OR_KEY_MISSING)
+    {
+      if (bleBondingHandle != 0xFF)
+      {
+        sl_app_log("Broken bond, deleting ID:%d...\r\n", bleBondingHandle);
+        sc = sl_bt_sm_delete_bonding(bleBondingHandle);
+        sl_app_assert(sc == SL_STATUS_OK,
+                      "[E: 0x%04x] Failed to delete specified bonding information or whitelist\r\n",
+                      (int)sc);
+
+        sc = sl_bt_sm_increase_security(evt->data.evt_connection_opened.connection);
+        sl_app_assert(sc == SL_STATUS_OK,
+                      "[E: 0x%04x] Failed to enhance the security\r\n",
+                      (int)sc);
+
+        bleBondingHandle = 0xFF;
+      }
+      else
+      {
+        sl_app_log("Increasing security in one second...\r\n");
+        uint16_t result;
+        result = sl_bt_sm_increase_security(connectionHandle);
+        sl_app_log("Result... = 0x%04X\r\n", result);
+        sl_app_assert(result == SL_STATUS_OK,
+                      "[E: 0x%04x] Failed to enhance the security\r\n",
+                      (int)sc);
+
+        if (result == SL_STATUS_INVALID_STATE)
+        {
+          sl_app_log("Trying to increase security again");
+          sc = sl_bt_sm_increase_security(connectionHandle);
+          sl_app_assert(sc == SL_STATUS_OK,
+                        "[E: 0x%04x] Failed to enhance the security\r\n",
+                        (int)sc);
+        }
+      }
+    }
+
+    if (reason == SL_STATUS_BT_SMP_UNSPECIFIED_REASON)
+    {
+      sl_app_log("Increasing security... because reason is 0x0308\r\n");
+      sl_bt_sm_increase_security(connectionHandle);
+      sl_app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to enhance the security\r\n",
+                    (int)sc);
+    }
+    break;
+  }
+
+  case sl_bt_evt_gatt_server_execute_write_completed_id:
+    sl_app_log("Bluetooth Stack Event : GATT SERVER WRITE COMPLETED\r\n");
+    break;
+
+  case sl_bt_evt_gatt_server_characteristic_status_id:
+    sl_app_log("Bluetooth Stack Event : GATT SERVER CHARACTERISTIC STATUS\r\n");
+    switch (evt->data.evt_gatt_server_characteristic_status.characteristic)
+    {
+    default:
+      sl_app_log("GATT unknown...\r\n");
+      break;
+    }
+    break;
+
+  case sl_bt_evt_gatt_server_attribute_value_id:
+    sl_app_log("Bluetooth Stack Event : GATT SERVER ATTRIBUTE VALUE\r\n");
+    switch (evt->data.evt_gatt_server_attribute_value.attribute)
+    {
+    default:
+      sl_app_log("unkown attribute???\r\n");
+      break;
+    }
+    break;
+
+  case sl_bt_evt_system_external_signal_id:
+    sl_app_log("Bluetooth Stack Event : EXTERNAL SIGNAL\r\n");
+    break;
+
+  case sl_bt_evt_gatt_server_user_write_request_id:
+    sl_app_log("Bluetooth Stack Event : GATT SERVER USER WRITE REQUEST\r\n");
+    break;
+
+  ///////////////////////////////////////////////////////////////////////////
+  // Add additional event handlers here as your application requires!      //
+  ///////////////////////////////////////////////////////////////////////////
+
+  // -------------------------------
+  // Default event handler.
+  default:
+    sl_app_log("Bluetooth Stack Event : UNKNOWN (0x%04lx)\r\n", SL_BT_MSG_ID(evt->header));
+    break;
+  }
+}

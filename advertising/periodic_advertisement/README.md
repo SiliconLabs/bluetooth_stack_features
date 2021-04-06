6e37a1d6569b3cb980b8e894fbad0e286c427b67
FSOFT-KHOIDT5 <72484061+FSOFT-KHOIDT5@users.noreply.github.com>
2021-04-07 02:27:12 +0700

    code example added: Periodic Advertisement

---
+++ advertising/periodic_advertisement/README.md
@@ -0,0 +1,121 @@
+# Periodic&nbsp;Advertisement&nbsp;Example
+
+## Description
+
+This example demonstrates the periodic advertising feature of Bluetooth 5, detailed in the [Periodic Advertising](https://docs.silabs.com/bluetooth/2.13/general/adv-and-scanning/periodic-adv-bt5) article. The example consists of two projects, one for the advertiser and one for the scanner.
+
+The advertiser starts both periodic advertising and extended advertisement to advertise the sync info needed for the periodic advertising. The advertiser changes the content of the periodic advertisement every second.
+
+The scanner starts scanning for the extended advertisements to find the sync info, and then syncs on the periodic advertising. Once synced, the scanning is stopped and only periodic advertisements are received.
+
+To start periodic advertisement, the advertiser uses the function:
+
+```C
+sl_bt_advertiser_start_periodic_advertising(advertising_set_handle,
+                                            160, // periodic interval min
+                                            160, // periodic interval max
+                                            1); //Include TX power in advertising PDU
+```
+
+To set the periodic advertisement data, the advertiser use the function:
+
+```C
+sl_bt_advertiser_set_data(advertising_set_handle,
+                          8, // 8 : Periodic advertising packets
+                          sizeof(periodic_adv_data),
+                          periodic_adv_data);
+```
+
+The scanner will find the periodic advertiser by the UUID of the Synchronous service, then start syncing with the advertiser by using the function:
+
+```C
+sl_bt_sync_open(evt->data.evt_scanner_scan_report.address,
+                evt->data.evt_scanner_scan_report.address_type,
+                evt->data.evt_scanner_scan_report.adv_sid,
+                &sync);
+```
+
+There are two important things when setting up this example in **Bluetooth SDK version 3.1**:
+
+- The first is to set the value **Max number of periodic advertising synchronizations** in **Bluetooth Core configure** on the **Scanner** according to the number of advertisers you want to sync on
+
+- The second is to install the **Periodic Advertising** component in the **Advertiser** and the **Periodic Advertising Synchronization** component in the **Scanner**.
+
+See section [Setting up](#setting-up) to see how to initialize the periodic advertisement in Bluetooth SDK version 3.1.
+
+## Gecko SDK version ##
+
+- GSDK v3.1.1
+
+## Hardware Required ##
+
+- 2 x WSTK board: BR4001A
+- 2 x Bluetooth radio board, e.g: BRD4162A
+
+## Setting up
+
+To try this example, you need two radio boards, one for the advertiser side and one for the scanner side.
+
+### Advertiser
+
+1. Create a new **SoC-Empty** project.
+
+2. Copy the attached [src/advertiser/app.c](src/advertiser/app.c) file replacing the existing `app.c`.
+
+3. Open the .slcp file of your project and open the Software Components tab  
+    - Install **Periodic Advertising** component  
+    ![Periodic Advertising](images/add_periodic_component.png)
+
+    - Install **IO Stream: USART** component with the default instance name: **vcom**  
+    ![install usart](images/add_log_1.png)
+    ![install usart](images/add_log_2.png)
+
+    - Enable **Virtual COM UART** in the **Board Control** component  
+    ![board control configure](images/add_log_3.png)  
+    ![board control configure](images/add_log_4.png)  
+
+    - Install the **Log** component (found under Bluetooth > Utility group)
+    ![add log driver](images/add_log_5.png)  
+
+4. Import the GATT configuration:
+    - Open the **Bluetooth GATT Configurator** under the **CONFIGURATION TOOLS** tab.
+    - Find the Import button and import the attached **gatt_configuration.btconf** file.
+    ![btc configure](images/btconf.png)
+    - Save the GATT configuration (Ctrl+S).
+
+5. **Save and close** then the tool will auto-generate to code.
+
+6. Build and flash the project to the **Advertiser** device.
+
+### Scanner
+
+1. Create an **Bluetooth - SoC Empty** example for the radio boards in Simplicity Studio.
+
+2. Copy the attached [src/scanner/app.c](src/scanner/app.c) replacing the existing `app.c`.
+
+3. Config **Software components**.  
+
+    * In **Bluetooth Core** configuration, set **Max number of periodic advertising synchronizations**. This example syncs to one advertiser only, but it is possible to sync to multiple advertisers at the same time.
+      ![install usart](images/periodic_ble_core_config_1.png)
+      ![install usart](images/periodic_ble_core_config_2.png)
+
+    * Install the **Periodic Advertising Synchronization** component.  
+      ![Periodic Advertising](images/add_periodic_sync_component.png)
+
+4. **Save and close** then the tool will auto-generate to code.
+
+5. Build and flash the project to the **Scanner** device.
+
+## Usage
+
+After flashing the applications to the devices, launch console for both **Advertiser** and **Scanner**, and reset both devices. On the logs, you should be able to observe that those devices are sending and receiving periodic advertisements after the scanner synced on the advertiser.
+
+![Logs of the advertiser and the scanner](images/result_1.png)
+
+The advertiser changes the content of the advertisement every second, but the periodic advertising interval is set as 200ms so each data will be received 5 times. Which is shown in the console.
+
+![Logs of the advertiser and the scanner](images/result_2.png)
+
+Use the energy profiler in Simplicity studio to evaluate the current consumption. The scanner goes into energy saving mode and wakes up every 200 ms to receive sync packets from the advertiser. The advertiser sleeps when not advertising, as shown in the figure below.
+
+![Periodic Advertisement Energy Profiler](images/figure_1.png)
---
+++ advertising/periodic_advertisement/src/advertiser/app.c
@@ -0,0 +1,199 @@
+/***************************************************************************//**
+ * @file
+ * @brief Core application logic.
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2020 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of Silicon Labs Master Software License
+ * Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement. This
+ * software is distributed to you in Source Code format and is governed by the
+ * sections of the MSLA applicable to Source Code.
+ *
+ ******************************************************************************/
+#include "em_common.h"
+#include "sl_app_assert.h"
+#include "sl_bluetooth.h"
+#include "gatt_db.h"
+#include "app.h"
+
+#include "sl_bt_api.h"
+#include "sl_app_log.h"
+
+// The advertising set handle allocated from Bluetooth stack.
+static uint8_t advertising_set_handle = 0xff;
+
+/**************************************************************************//**
+ * Application Init.
+ *****************************************************************************/
+SL_WEAK void app_init(void)
+{
+  /////////////////////////////////////////////////////////////////////////////
+  // Put your additional application init code here!                         //
+  // This is called once during start-up.                                    //
+  /////////////////////////////////////////////////////////////////////////////
+}
+
+/**************************************************************************//**
+ * Application Process Action.
+ *****************************************************************************/
+SL_WEAK void app_process_action(void)
+{
+  /////////////////////////////////////////////////////////////////////////////
+  // Put your additional application code here!                              //
+  // This is called infinitely.                                              //
+  // Do not call blocking functions from here!                               //
+  /////////////////////////////////////////////////////////////////////////////
+}
+
+/**************************************************************************//**
+ * Bluetooth stack event handler.
+ * This overrides the dummy weak implementation.
+ *
+ * @param[in] evt Event coming from the Bluetooth stack.
+ *****************************************************************************/
+void sl_bt_on_event(sl_bt_msg_t *evt)
+{
+  sl_status_t sc;
+  bd_addr address;
+  uint8_t address_type;
+  uint8_t system_id[8];
+
+  static uint8_t periodic_adv_data[191];
+  int16_t result;
+
+  switch (SL_BT_MSG_ID(evt->header)) {
+    // -------------------------------
+    // This event indicates the device has started and the radio is ready.
+    // Do not call any stack command before receiving this boot event!
+    case sl_bt_evt_system_boot_id:
+      // Extract unique ID from BT Address.
+      sc = sl_bt_system_get_identity_address(&address, &address_type);
+      sl_app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to get Bluetooth address\n",
+                    (int)sc);
+
+      sl_app_log("Bluetooth %s address: %02X:%02X:%02X:%02X:%02X:%02X\n",
+                 address_type ? "static random" : "public device",
+                 address.addr[5],
+                 address.addr[4],
+                 address.addr[3],
+                 address.addr[2],
+                 address.addr[1],
+                 address.addr[0]);
+      // Pad and reverse unique ID to get System ID.
+      system_id[0] = address.addr[5];
+      system_id[1] = address.addr[4];
+      system_id[2] = address.addr[3];
+      system_id[3] = 0xFF;
+      system_id[4] = 0xFE;
+      system_id[5] = address.addr[2];
+      system_id[6] = address.addr[1];
+      system_id[7] = address.addr[0];
+      sc = sl_bt_gatt_server_write_attribute_value(gattdb_system_id,
+                                                   0,
+                                                   sizeof(system_id),
+                                                   system_id);
+      sl_app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to write attribute\n",
+                    (int)sc);
+
+      // Create an advertising set.
+      sc = sl_bt_advertiser_create_set(&advertising_set_handle);
+      sl_app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to create advertising set\n",
+                    (int)sc);
+
+      // Set TX power
+      sc = sl_bt_advertiser_set_tx_power(advertising_set_handle, 30, &result);
+      sl_app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to set power\n",
+                    (int)sc);
+
+      // Do not use legacy PDU - use extended advertisement instead.
+      sc = sl_bt_advertiser_clear_configuration(advertising_set_handle,
+                                                1);
+      sl_app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to set advertising configuration\n",
+                    (int)sc);
+
+      // Set advertising interval to 100ms.
+      sc = sl_bt_advertiser_set_timing(advertising_set_handle,
+                                       160, // min. adv. interval. Value in units of 0.625 ms
+                                       160, // max. adv. interval. Value in units of 0.625 ms
+                                       0,   // adv. duration
+                                       0);  // max. num. adv. events
+      sl_app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to set advertising timing\n",
+                    (int)sc);
+
+      // Start general advertising
+      sc = sl_bt_advertiser_start(advertising_set_handle,
+                                  advertiser_general_discoverable,
+                                  advertiser_non_connectable);
+      sl_app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to start advertising\n",
+                    (int)sc);
+
+      // Start periodic advertising with periodic interval 200ms
+      sc = sl_bt_advertiser_start_periodic_advertising(advertising_set_handle,
+                                                       160, //min periodic advertising interval. Value in units of 1.25 ms
+                                                       160, //max periodic advertising interval. Value in units of 1.25 ms
+                                                       1);  //include TX power in advertising PDU
+      sl_app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to start periodic advertising\n",
+                    (int)sc);
+
+      // Set the advertising data
+      sc = sl_bt_advertiser_set_data(advertising_set_handle,
+                                8,
+                                sizeof(periodic_adv_data),
+                                periodic_adv_data);
+      sl_app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to set adv data\n",
+                    (int)sc);
+
+      // Start soft timer to change advertising data every 1s
+      sc = sl_bt_system_set_soft_timer(32768, // 32768 ticks for 1 second
+                                       0,     // soft timer handle
+                                       0);    // single_shot = false
+      sl_app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to set soft-timer\n",
+                    (int)sc);
+      break;
+
+    // -------------------------------
+    // This event indicates that a new connection was opened.
+    case sl_bt_evt_connection_opened_id:
+      break;
+
+    // Change advertisement data
+    case sl_bt_evt_system_soft_timer_id:
+      sl_app_log("\r\n");
+      for (int i = 0; i < 190; i++) {
+          periodic_adv_data[i] = rand()%9;
+          sl_app_log(" %X", periodic_adv_data[i]);
+      }
+      sl_app_log("\r\n");
+      sc = sl_bt_advertiser_set_data(advertising_set_handle,
+                                8,
+                                sizeof(periodic_adv_data),
+                                periodic_adv_data);
+      sl_app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to set adv data\n",
+                    (int)sc);
+      break;
+    ///////////////////////////////////////////////////////////////////////////
+    // Add additional event handlers here as your application requires!      //
+    ///////////////////////////////////////////////////////////////////////////
+
+    // -------------------------------
+    // Default event handler.
+    default:
+      break;
+  }
+}
+

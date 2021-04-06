90816869630525484ddb32138162bafdc02fce52
FSOFT-VINHPT9 <79197973+FSOFT-VINHPT9@users.noreply.github.com>
2021-04-07 02:34:16 +0700

    code example added: Multiple Advertising Sets

---
+++ advertising/multiple-adv-sets-bt5/README.md
@@ -0,0 +1,108 @@
+# Multiple Advertising Sets
+
+## Description
+
+Bluetooth 5 introduces a number of useful new features, such as multiple advertising sets. This feature supports having  more than one advertising sets concurrently without having to stop and restart advertising.
+
+All parameters including the advertising interval, advertising data, discoverability, connectability and number of advertisements to send before stopping can be configured independently for each advertising set. The most relevant APIs for configuring advertising sets are discussed below.
+
+### Maximum Advertising Sets
+
+To enable multiple advertising, the `Max number of advertisers reserved for user` value in **Bluetooth Core** should be set greater than one. The default value is 1.
+
+### Creating a New Advertising Set
+
+Before an advertising set could be used, it must be initialized with `sl_bt_advertiser_create_set(&handle)`. The Bluetooth stack reserves the required memory for the advertising set, and returns a new valid advertising handle to the variable passed as an argument. This handle can be used later to refer to a given advertising set.
+
+### Advertising Data
+
+To set the advertising data, use the API `sl_bt_advertiser_set_data(handle, packet_type, adv_data_len, adv_data)`, where:
+
+- `handle`: advertising set handle
+- `packet_type`: tells whether the data is
+    - **0**: Advertising packets
+    - **1**: Scan response packets
+    - **8**: Periodic advertising packets
+- `adv_data_len`: array length
+- `adv_data`: data to be set
+
+### Maximum Advertising Power
+
+Users can also set the maximum advertising TX power on a given advertising set by calling the API `sl_bt_advertiser_set_tx_power(handle, power, *set_power)`, where `power` is the TX power in units of 0.1 dBm on a given advertising set specified by `handle`. `set_power` is the selected maximum advertising TX power. For instance, value of 80 sets the TX power to 8dBm.
+
+*Note that if the TX power value goes over the global value that was set using `sl_bt_system_set_tx_power` command, the global value will be the maximum limit.*
+
+### Advertising Parameters
+
+To control advertising parameters , use `sl_bt_advertiser_set_timing(handle, interval_min, interval_max, duration, maxevents)`, where:
+
+- `handle`: advertising set handle
+- `interval_min`: minimum advertising interval in units of 0.625 ms
+- `interval_max`: maximum advertising interval in units of 0.625 ms
+- `duration`: advertising duration in units of 10 ms for the advertising set specified by `handle`
+- `maxevents`: the maximum number of advertising events to send before advertiser is stopped.
+
+### Start Advertising
+
+Finally, advertising on a given advertising set can be started using `sl_bt_advertiser_start(handle, discover, connect)`, where
+
+- `discover`: the discoverable mode of the advertising set
+- `connect`: the connectable mode of the advertising set
+
+Note that the number of concurrent advertising is limited by the `Max number of advertisers reserved for user` configuration discussed above. Similarly, the number of **connectable** advertising is also limited by `Max number of connections` configuration. For example, only one connectable advertising can be enabled if the device has (`Max number of connections` - 1) connections when this command is called. The limitation does not apply to **non-connectable** advertising, though.
+
+The example code below demonstrates multiple advertising features in Bluetooth 5. Two advertising sets, one connectable, and another non-connectable (iBeacon), are configured separately using their respective handles. The advertising interval and TX power values used for each advertising set is different so that they can easily be distinguished from the Energy Profile perspective in Simplicity Studio.
+
+In addition to debug print out messages, LED0 is used to indicate the connection status. Follow the instructions below and verify the result using the Energy Profile perspective in Simplicity Studio, and the LED0 status on the controller board.
+
+## Gecko SDK version ##
+
+GSDK v3.1.1
+
+## Hardware Required ##
+
+- One WSTK board
+- One Bluetooth capable radio board, e.g: BRD4162A
+
+## Setting up
+
+1. Create a **Bluetooth - SoC Empty** project
+
+2. Install the software components to use the **VCOM** port (UART) for logging:
+   - Open the .slcp file in the project.
+
+   - Select the SOFTWARE COMPONENTS tab.
+
+   - Install **IO Stream: USART** component with the default instance name: **vcom**.
+    ![vcom_component](images/install_usart.png)
+
+   - Find the **Board Control** component and click to the **Configure** button like below
+   ![board control configure](images/board_control_configure.png)  
+   Then enable *Virtual COM UART* under its configuration
+   ![vcom configure](images/enable_vir_com.png)
+
+   - Install the **Log** component (found under Bluetooth > Utility group).  
+    ![log configure](images/log.png)
+
+3. Set the `Max number of advertisers reserved for user` value in **Bluetooth Core** to 2 for multiple advertising.
+![log configure](images/adv.png)
+
+4. Install the **Simple LED** component with the default instance name: **led0**
+   ![led0](images/led0.png)
+
+5. Replace the *app.c* file in the project with the provided app.c.
+
+5. Build and flash them to each device.
+
+
+## Usage
+
+1. Open *Energy Profiler* perspective in Simplicity Studio and *Start Energy Capture* on your device (Quick Access menu). You should be able to see the following linear plot for the current consumption of your device.
+
+   ![](images/fig_1.png)
+
+2. Open EFR Connect app and try to connect with your target device. LED0 will be ON when connection is established.  Meanwhile, beacons are still broadcast.
+
+3. Disconnect the connection and the advertising should start again. The following figure shows the debug print out messages.
+
+   ![](images/fig_2.png)
---
+++ advertising/multiple-adv-sets-bt5/src/app.c
@@ -0,0 +1,263 @@
+/***************************************************************************/ /**
+ * @file
+ * @brief Core application logic.
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2020 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of Silicon Labs Master Software License
+ * Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement. This
+ * software is distributed to you in Source Code format and is governed by the
+ * sections of the MSLA applicable to Source Code.
+ *
+ ******************************************************************************/
+#include "em_common.h"
+#include "sl_app_assert.h"
+#include "sl_bluetooth.h"
+#include "gatt_db.h"
+#include "app.h"
+#include "sl_app_log.h"
+#include "sl_simple_led_instances.h"
+
+#define UINT16_TO_BYTES(n) ((uint8_t)(n)), ((uint8_t)((n) >> 8))
+#define UINT16_TO_BYTE0(n) ((uint8_t)(n))
+#define UINT16_TO_BYTE1(n) ((uint8_t)((n) >> 8))
+// The advertising set handle allocated from Bluetooth stack.
+static uint8_t handle_ibeacon;
+static uint8_t handle_demo;
+void bcnSetupAdvBeaconing(void);
+/**************************************************************************/ /**
+ * Application Init.
+ *****************************************************************************/
+SL_WEAK void app_init(void)
+{
+  /////////////////////////////////////////////////////////////////////////////
+  // Put your additional application init code here!                         //
+  // This is called once during start-up.                                    //
+  /////////////////////////////////////////////////////////////////////////////
+}
+
+/**************************************************************************/ /**
+ * Application Process Action.
+ *****************************************************************************/
+SL_WEAK void app_process_action(void)
+{
+  /////////////////////////////////////////////////////////////////////////////
+  // Put your additional application code here!                              //
+  // This is called infinitely.                                              //
+  // Do not call blocking functions from here!                               //
+  /////////////////////////////////////////////////////////////////////////////
+}
+
+/**************************************************************************/ /**
+ * Bluetooth stack event handler.
+ * This overrides the dummy weak implementation.
+ *
+ * @param[in] evt Event coming from the Bluetooth stack.
+ *****************************************************************************/
+void sl_bt_on_event(sl_bt_msg_t *evt)
+{
+  sl_status_t sc;
+  bd_addr address;
+  uint8_t address_type;
+  uint8_t system_id[8];
+
+  switch (SL_BT_MSG_ID(evt->header))
+  {
+  // -------------------------------
+  // This event indicates the device has started and the radio is ready.
+  // Do not call any stack command before receiving this boot event!
+  case sl_bt_evt_system_boot_id:
+
+    // Extract unique ID from BT Address.
+    sc = sl_bt_system_get_identity_address(&address, &address_type);
+    sl_app_assert(sc == SL_STATUS_OK,
+                  "[E: 0x%04x] Failed to get Bluetooth address\n",
+                  (int)sc);
+
+    // Pad and reverse unique ID to get System ID.
+    system_id[0] = address.addr[5];
+    system_id[1] = address.addr[4];
+    system_id[2] = address.addr[3];
+    system_id[3] = 0xFF;
+    system_id[4] = 0xFE;
+    system_id[5] = address.addr[2];
+    system_id[6] = address.addr[1];
+    system_id[7] = address.addr[0];
+
+    sc = sl_bt_gatt_server_write_attribute_value(gattdb_system_id,
+                                                 0,
+                                                 sizeof(system_id),
+                                                 system_id);
+    sl_app_assert(sc == SL_STATUS_OK,
+                  "[E: 0x%04x] Failed to write attribute\n",
+                  (int)sc);
+
+    // Create an advertising set.
+    sc = sl_bt_advertiser_create_set(&handle_demo);
+    sl_app_assert(sc == SL_STATUS_OK,
+                  "[E: 0x%04x] Failed to create advertising set\n",
+                  (int)sc);
+
+    // Set advertising interval to 100ms.
+    sc = sl_bt_advertiser_set_timing(
+        handle_demo,
+        160, // min. adv. interval (milliseconds * 1.6)
+        160, // max. adv. interval (milliseconds * 1.6)
+        0,   // adv. duration
+        0);  // max. num. adv. events
+    sl_app_assert(sc == SL_STATUS_OK,
+                  "[E: 0x%04x] Failed to set advertising timing\n",
+                  (int)sc);
+
+    sl_bt_system_set_tx_power(0, 100, NULL, NULL);
+    sl_app_assert(sc == SL_STATUS_OK,
+                  "[E: 0x%04x] Failed to set the global minimum and maximum radiated TX power levels for Bluetooth\n",
+                  (int)sc);
+
+    /* Set 1 dBm Transmit Power */
+    sl_bt_advertiser_set_tx_power(handle_demo, 10, NULL);
+    sl_app_assert(sc == SL_STATUS_OK,
+                  "[E: 0x%04x] Failed to limit the maximum advertising TX power\n",
+                  (int)sc);
+
+    // Start general advertising and enable connections.
+    sc = sl_bt_advertiser_start(
+        handle_demo,
+        advertiser_general_discoverable,
+        advertiser_connectable_scannable);
+    sl_app_assert(sc == SL_STATUS_OK,
+                  "[E: 0x%04x] Failed to start advertising\n",
+                  (int)sc);
+
+    sl_app_log("\r\nFirst connectable advertising set started.\r\n");
+    sl_app_log("  Conected-> LED0 ON else LED0->OFF\r\n");
+    bcnSetupAdvBeaconing();
+    sl_app_log("\r\nSecond non-connectable advertising set started.\r\n");
+    break;
+
+  // -------------------------------
+  // This event indicates that a new connection was opened.
+  case sl_bt_evt_connection_opened_id:
+    sl_led_led0.turn_on(sl_led_led0.context);
+    sl_app_log("\r\n connection opened. LED0 status: %s \r\n", sl_led_led0.get_state(sl_led_led0.context) == 1 ? "ON" : "OFF");
+    break;
+
+  // -------------------------------
+  // This event indicates that a connection was closed.
+  case sl_bt_evt_connection_closed_id:
+    sl_led_led0.turn_off(sl_led_led0.context);
+    sl_app_log("\r\n connection opened. LED0 status: %s \r\n", sl_led_led0.get_state(sl_led_led0.context) == 1 ? "ON" : "OFF");
+    // Restart advertising after client has disconnected.
+    sc = sl_bt_advertiser_start(
+        handle_demo,
+        advertiser_general_discoverable,
+        advertiser_connectable_scannable);
+    sl_app_assert(sc == SL_STATUS_OK,
+                  "[E: 0x%04x] Failed to start advertising\n",
+                  (int)sc);
+    break;
+
+  ///////////////////////////////////////////////////////////////////////////
+  // Add additional event handlers here as your application requires!      //
+  ///////////////////////////////////////////////////////////////////////////
+
+  // -------------------------------
+  // Default event handler.
+  default:
+    break;
+  }
+}
+void bcnSetupAdvBeaconing(void)
+{
+  sl_status_t sc;
+
+  /* This function sets up a custom advertisement package according to iBeacon specifications.
+   * The advertisement package is 30 bytes long. See the iBeacon specification for further details.
+   */
+
+  static struct
+  {
+    uint8_t flagsLen;    /* Length of the Flags field. */
+    uint8_t flagsType;   /* Type of the Flags field. */
+    uint8_t flags;       /* Flags field. */
+    uint8_t mandataLen;  /* Length of the Manufacturer Data field. */
+    uint8_t mandataType; /* Type of the Manufacturer Data field. */
+    uint8_t compId[2];   /* Company ID field. */
+    uint8_t beacType[2]; /* Beacon Type field. */
+    uint8_t uuid[16];    /* 128-bit Universally Unique Identifier (UUID). The UUID is an identifier for the company using the beacon*/
+    uint8_t majNum[2];   /* Beacon major number. Used to group related beacons. */
+    uint8_t minNum[2];   /* Beacon minor number. Used to specify individual beacons within a group.*/
+    uint8_t txPower;     /* The Beacon's measured RSSI at 1 meter distance in dBm. See the iBeacon specification for measurement guidelines. */
+  } bcnBeaconAdvData = {
+      /* Flag bits - See Bluetooth 4.0 Core Specification , Volume 3, Appendix C, 18.1 for more details on flags. */
+      2,           /* length  */
+      0x01,        /* type */
+      0x04 | 0x02, /* Flags: LE General Discoverable Mode, BR/EDR is disabled. */
+
+      /* Manufacturer specific data */
+      26,   /* length of field*/
+      0xFF, /* type of field */
+
+      /* The first two data octets shall contain a company identifier code from
+   * the Assigned Numbers - Company Identifiers document */
+      /* 0x004C = Apple */
+      {UINT16_TO_BYTES(0x004C)},
+
+      /* Beacon type */
+      /* 0x0215 is iBeacon */
+      {UINT16_TO_BYTE1(0x0215), UINT16_TO_BYTE0(0x0215)},
+
+      /* 128 bit / 16 byte UUID */
+      {0xE2, 0xC5, 0x6D, 0xB5, 0xDF, 0xFB, 0x48, 0xD2, 0xB0, 0x60, 0xD0, 0xF5, 0xA7, 0x10, 0x96, 0xE0},
+
+      /* Beacon major number */
+      /* Set to 34987 and converted to correct format */
+      {UINT16_TO_BYTE1(34987), UINT16_TO_BYTE0(34987)},
+
+      /* Beacon minor number */
+      /* Set as 1025 and converted to correct format */
+      {UINT16_TO_BYTE1(1025), UINT16_TO_BYTE0(1025)},
+
+      /* The Beacon's measured RSSI at 1 meter distance in dBm */
+      /* 0xC3 is -61dBm */
+      0xC3};
+
+  //
+  uint8_t len = sizeof(bcnBeaconAdvData);
+  uint8_t *pData = (uint8_t *)(&bcnBeaconAdvData);
+
+  /* Set custom advertising data */
+  sc = sl_bt_advertiser_create_set(&handle_ibeacon);
+  sl_app_assert(sc == SL_STATUS_OK,
+                "[E: 0x%04x] Failed to create advertising set\n",
+                (int)sc);
+
+  sl_bt_advertiser_set_data(handle_ibeacon, 0, len, pData);
+  sl_app_assert(sc == SL_STATUS_OK,
+                "[E: 0x%04x] Failed to set user-defined data\n",
+                (int)sc);
+
+  /* Set 8 dBm Transmit Power */
+  sl_bt_advertiser_set_tx_power(handle_ibeacon, 80, NULL);
+  sl_app_assert(sc == SL_STATUS_OK,
+                "[E: 0x%04x] Failed to limit the maximum advertising TX power\n",
+                (int)sc);
+
+  /* Set advertising parameters. 200ms (320/1.6) advertisement interval. All channels used.
+   * The first two parameters are minimum and maximum advertising interval, both in
+   * units of (milliseconds * 1.6).  */
+  sc = sl_bt_advertiser_set_timing(handle_ibeacon, 320, 320, 0, 0);
+  sl_app_assert(sc == SL_STATUS_OK,
+                "[E: 0x%04x] Failed to set advertising timing\n",
+                (int)sc);
+
+  /* Start advertising in user mode */
+  sc = sl_bt_advertiser_start(handle_ibeacon, sl_bt_advertiser_user_data, sl_bt_advertiser_non_connectable);
+  sl_app_assert(sc == SL_STATUS_OK,
+                "[E: 0x%04x] Failed to start advertising\n",
+                (int)sc);
+}

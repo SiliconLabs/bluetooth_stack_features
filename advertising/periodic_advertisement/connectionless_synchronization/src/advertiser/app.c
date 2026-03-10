93499a403588c46082b3284d94c0a7146c7a67f6
silabs-mehdi <mehdi.marzougui@silab.com>
2026-03-10 12:34:50 +0100

    Periodic advertisement connectionless sync

--- advertising/periodic_advertisement/connectionless_synchronization/src/advertiser/app.c
+++ advertising/periodic_advertisement/connectionless_synchronization/src/advertiser/app.c
@@ -19,18 +19,21 @@
 #include "sl_bluetooth.h"
 #include "gatt_db.h"
 #include "app.h"
-
 #include "sl_bt_api.h"
 #include "app_log.h"
 
-#define SIGNAL_REFRESH_DATA   1
-#define TICKS_PER_SECOND      32768
+#define SIGNAL_REFRESH_DATA   1 //bitmask used to report external signal to the ble stack
+#define TICKS_TIMEOUT      32768 // Timer periodic timeout, in timer ticks
+#define TX_POWER 30 //effective value TX_POWER/10
 
 // The advertising set handle allocated from Bluetooth stack.
 static uint8_t advertising_set_handle = 0xff;
-
-sl_sleeptimer_timer_handle_t sleep_timer_handle;
+// Periodic advertisement data
+static uint8_t periodic_adv_data[8] = { 0x07, 0xFF, 0x02, 0xFF, 0x00, 0x00, 0x00, 0x00 };
+//Sleeptimer used to update periodic data
+static sl_sleeptimer_timer_handle_t sleep_timer_handle;
 void sleeptimer_callback(sl_sleeptimer_timer_handle_t *handle, void *data);
+
 /**************************************************************************//**
  * Application Init.
  *****************************************************************************/
@@ -63,23 +66,21 @@ SL_WEAK void app_process_action(void)
 void sl_bt_on_event(sl_bt_msg_t *evt)
 {
   sl_status_t sc;
-  bd_addr address;
-  uint8_t address_type;
-  uint8_t system_id[8];
-
-  static uint8_t periodic_adv_data[191];
-  int16_t result;
 
   switch (SL_BT_MSG_ID(evt->header)) {
     // -------------------------------
     // This event indicates the device has started and the radio is ready.
     // Do not call any stack command before receiving this boot event!
-    case sl_bt_evt_system_boot_id:
+    case sl_bt_evt_system_boot_id: {
+      bd_addr address;
+      uint8_t address_type;
+      uint8_t system_id[8];
+      int16_t result;
       // Extract unique ID from BT Address.
       sc = sl_bt_system_get_identity_address(&address, &address_type);
       app_assert_status(sc);
 
-      app_log_info("Bluetooth %s address: %02X:%02X:%02X:%02X:%02X:%02X\n",
+      app_log_info("Bluetooth %s address: %02X:%02X:%02X:%02X:%02X:%02X\r\n",
                    address_type ? "static random" : "public device",
                    address.addr[5],
                    address.addr[4],
@@ -87,7 +88,7 @@ void sl_bt_on_event(sl_bt_msg_t *evt)
                    address.addr[2],
                    address.addr[1],
                    address.addr[0]);
-      // Pad and reverse unique ID to get System ID.
+      // Pad and reverse unique ID to get System ID, write it to the corresponding GATT characteristic
       system_id[0] = address.addr[5];
       system_id[1] = address.addr[4];
       system_id[2] = address.addr[3];
@@ -107,10 +108,9 @@ void sl_bt_on_event(sl_bt_msg_t *evt)
       app_assert_status(sc);
 
       // Set TX power
-      sc = sl_bt_advertiser_set_tx_power(advertising_set_handle, 30, &result);
+      sc = sl_bt_advertiser_set_tx_power(advertising_set_handle, TX_POWER, &result);
       app_assert_status(sc);
-
-      app_assert_status(sc);
+      app_log_info("Tx power of advertising set to %d.%d dBm\r\n", result / 10, result % 10);
 
       // Set advertising interval to 100ms.
       sc = sl_bt_advertiser_set_timing(advertising_set_handle,
@@ -120,10 +120,9 @@ void sl_bt_on_event(sl_bt_msg_t *evt)
                                        0);  // max. num. adv. events
       app_assert_status(sc);
 
-      // Start general advertising
+      // Start general  extended advertising
       sc = sl_bt_extended_advertiser_generate_data(advertising_set_handle,
                                                    sl_bt_advertiser_general_discoverable);
-
       app_assert_status(sc);
 
       sc = sl_bt_extended_advertiser_start(advertising_set_handle,
@@ -132,36 +131,39 @@ void sl_bt_on_event(sl_bt_msg_t *evt)
 
       app_assert_status(sc);
 
-      // Start periodic advertising with periodic interval 200ms
-      sc = sl_bt_periodic_advertiser_start(advertising_set_handle, 160, 160,
-                                           SL_BT_PERIODIC_ADVERTISER_AUTO_START_EXTENDED_ADVERTISING);
+      // Start periodic advertising with periodic interval 500ms
+      sc = sl_bt_periodic_advertiser_start(advertising_set_handle,
+                                           400,
+                                           400,
+                                           0);
       app_assert_status(sc);
 
-      // Set the advertising data
       sc = sl_bt_periodic_advertiser_set_data(advertising_set_handle, sizeof(periodic_adv_data), periodic_adv_data);
-
       app_assert_status(sc);
 
       // Start a timer to change advertising data every 1s
-      sc = sl_sleeptimer_start_periodic_timer(&sleep_timer_handle, TICKS_PER_SECOND, sleeptimer_callback, (void*)NULL, 0, 0);
-
+      sc = sl_sleeptimer_start_periodic_timer(&sleep_timer_handle, TICKS_TIMEOUT, sleeptimer_callback, (void*)NULL, 0, 0);
       app_assert_status(sc);
       break;
+    }
 
     // -------------------------------
     // This event indicates that a new connection was opened.
     case sl_bt_evt_connection_opened_id:
       break;
 
+    // -------------------------------
+    // This event indicates that an external has been generated
     case sl_bt_evt_system_external_signal_id:
       if (evt->data.evt_system_external_signal.extsignals == SIGNAL_REFRESH_DATA) {
-        app_log("\r\n");
-        for (int i = 0; i < 190; i++) {
+        app_log_info("Periodic advertisement payload:  ");
+        for (uint8_t i = 4; i < 8; i++) {
           periodic_adv_data[i] = rand() % 9;
-          app_log(" %X", periodic_adv_data[i]);
+        }
+        for (uint8_t i = 0; i < 8; i++) {
+          app_log("%02X,", periodic_adv_data[i]);
         }
         app_log("\r\n");
-
         sc = sl_bt_periodic_advertiser_set_data(advertising_set_handle, sizeof(periodic_adv_data), periodic_adv_data);
         app_assert_status(sc);
       }
